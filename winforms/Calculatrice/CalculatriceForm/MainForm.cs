bbda639b3967bb8cb3292b627003eaed99ababb4
MDevoldere <mdevoldere@users.noreply.github.com>
2022-06-08 16:12:31 +0200

    Refactor display code

--- winforms/Calculatrice/CalculatriceForm/MainForm.cs
+++ winforms/Calculatrice/CalculatriceForm/MainForm.cs
@@ -21,19 +21,38 @@ namespace CalculatriceForm
             if (sender is Button button)
             {
                 anOperator.AdditionNumber(Int32.Parse(button.Text));
-                TbResult.Text = TbResult.Text + "+" + button.Text;
+                DisplayAddition(button.Text);
             }
         }
 
         private void BResult_Click(object sender, EventArgs e)
         {
-            TbResult.Text += " = " + anOperator.GetResult().ToString();
+            DisplayResult();
         }
 
         private void BClear_Click(object sender, EventArgs e)
         {
             anOperator = new Operator();
+            ClearDisplay();
+        }
+
+        #region Display
+
+        private void DisplayAddition(string number)
+        {
+            TbResult.Text = TbResult.Text + "+" + number;
+        }
+
+        private void DisplayResult()
+        {
+            TbResult.Text += " = " + anOperator.GetResult().ToString();
+        }
+
+        private void ClearDisplay()
+        {
             TbResult.Text = String.Empty;
         }
+
+        #endregion Display
     }
 }
