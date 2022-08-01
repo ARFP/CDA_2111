27a53f89fbca90bf5859101e40f276775ee6c308
MDevoldere <mdevoldere@users.noreply.github.com>
2022-08-01 12:55:30 +0200

    Add example for parsing DateTime from Strings

---
+++ winforms/ConsoleDates/ConsoleDates/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+
+
+string date = "dd/MM/yyyy";
+Console.WriteLine("veuillez entrer une date au format : " + date );
+string inputDate = Console.ReadLine();
+
+try
+{
+    DateTime dt = DateTime.ParseExact(inputDate, date, CultureInfo.InvariantCulture);
+    Console.WriteLine("la date est valide : " + dt.ToShortDateString() + " et " + dt.ToLongDateString());
+}
+catch (ArgumentNullException e)
+{
+    Console.WriteLine("la date ne peut être vide");
+}
+catch (FormatException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
---
+++ winforms/ConsoleDates/ConsoleDatesTryParse/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("Hello!");
+
+Console.WriteLine("Veuillez entrer une date :");
+string? userDate = Console.ReadLine();
+
+CultureInfo frenchFormat = new CultureInfo("fr-FR");
+
+try
+{
+    DateTime date = DateTime.Parse(userDate, frenchFormat);
+
+    Console.WriteLine($"Votre date est : {date.ToString()}");
+}
+catch (ArgumentNullException e)
+{
+    Console.WriteLine("la date ne peut être vide");
+}
+catch (FormatException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
